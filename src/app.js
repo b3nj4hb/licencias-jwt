2f3f8966310effdd95d9e1a7bd9b69eec950584a
b3nj4hb <benjalive12@gmail.com>
2022-07-21 22:19:09 -0500

    sin import

--- src/app.js
+++ src/app.js
@@ -1,18 +1,18 @@
 import express from 'express'
 import morgan from 'morgan'
 
-import auth from './routes/auth.routes'
-
-// const app = express();
+// import auth from './routes/auth.routes'
+import cors from 'cors'
+const app = express();
 // var cors = require('cors');
-// app.use(express.json());
-// app.use(cors());
-// app.use(morgan('dev'));
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
 
-// app.get('/', (req, res) => {
-//     res.send('Bienvenido a Node JS..!');
-// });
+app.get('/', (req, res) => {
+    res.send('Bienvenido a Node JS..!');
+});
 
-app.use('/api/auth', auth)
+// app.use('/api/auth', auth)
 
 export default app;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
-// import app from './app.js'
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, function () {
-//     console.log("Servidor escuchando en el puerto: ", PORT)
-// });
-
-const express = require('express') ;
-const app = express();
-var cors = require('cors');
-app.use(express.json());
-app.use(cors());
-
-app.get('/',(req,res)=>{
-    res.send('Bienvenido a Node JS...!');
-
-});
-
+import app from './app.js'
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, function () {
-    console.log('server listen on port: ', PORT);
+    console.log("Servidor escuchando en el puerto: ", PORT)
 });
+
+// const express = require('express') ;
+// const app = express();
+// var cors = require('cors');
+// app.use(express.json());
+// app.use(cors());
+
+// app.get('/',(req,res)=>{
+//     res.send('Bienvenido a Node JS...!');
+// });
+// // import auth from './routes/auth.routes'
+// const auth = require('./routes/auth.routes')
+// app.use('/api/auth', auth)
+
+// const PORT = process.env.PORT || 3000;
+
+// app.listen(PORT, function () {
+//     console.log('server listen on port: ', PORT);
+// });
